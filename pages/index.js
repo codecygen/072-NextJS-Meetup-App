0635c72cad65f3d343a26533ecc567b8e4846c4f
gingercook <vahitaras@gmail.com>
2022-04-26 21:42:00 -0400

    meetup component added to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,29 @@
 // our-domain.com
+
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'First Meetup',
+        image: 'https://cdn.pixabay.com/photo/2014/07/01/13/52/toronto-381281_960_720.jpg',
+        address: '111 Wellesley St W, Toronto, ON M7A 1A2',
+        description: 'This is our first meetup!'
+    },
+
+    {
+        id: 'm2',
+        title: 'Second Meetup',
+        image: 'https://cdn.pixabay.com/photo/2015/11/07/11/00/toronto-city-hall-1030731_960_720.jpg',
+        address: '100 Queen St W, Toronto, ON M5H 2N2',
+        description: 'This is our second meetup!'
+    }
+];
+
+const HomePage = () => {
+    return (
+        <MeetupList meetups={DUMMY_MEETUPS} />
+    );
+};
+
+export default HomePage;
